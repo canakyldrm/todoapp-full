1bf5ee3de1ec03688ef149dd57c7fd2d188db8bd
Can <akyildirimcan@gmail.com>
2023-01-25 18:11:30 +0300

    prod database change

--- database.py
+++ database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-SQLALCHEMY_DATABASE_URL = "postgres://tfvlhcqm:FoeFars7s6HwT3XoxAvbJAXxkBw_uwH_@hattie.db.elephantsql.com/tfvlhcqm"
+
+SQLALCHEMY_DATABASE_URL = "postgresql://tfvlhcqm:FoeFars7s6HwT3XoxAvbJAXxkBw_uwH_@hattie.db.elephantsql.com/tfvlhcqm"
+
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
@@ -16,6 +18,7 @@ SQLALCHEMY_DATABASE_URL = "postgres://tfvlhcqm:FoeFars7s6HwT3XoxAvbJAXxkBw_uwH_@
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL
 # )
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
